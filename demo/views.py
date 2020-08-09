a4fbdf2307f05c9010405c59db1e813b8b8a19a8
Hung Ly <hunghly@gmail.com>
2020-08-09 02:27:38 -0500

    worked on model object

--- demo/views.py
+++ demo/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
-
+from .models import Book as B
 
 
 # Adding a class
 class Another(View):
 
+    books = B.objects.all()
+
+    output = f
+
     def get(self, request):
+        print(self.books)
+        print(self.books[0])
         return HttpResponse("This is another function inside class")
 
     def hello(self, request):
