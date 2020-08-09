096769602969489179bf1b3ac1e711415ab4e77c
Hung Ly <hunghly@gmail.com>
2020-08-09 00:27:46 -0500

    removed old repo and added new repo

---
+++ demo/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    STATUS = (
+        (0, 'Unknown'),
+        (1, 'processed'),
+        (2, 'paid')
+    )
+    GENRE = (
+        ("AC", "Action"),
+        ("FAN", "Fantasy"),
+        ("NF", "Non-fiction"),
+        ("FC", "Fiction")
+    )
+    FRIENDLY = (
+       ("KF", "Kid-Friendly"),
+       ("NKF", "Not-Kid-Friendly") 
+    )
+    title = models.CharField(max_length=36, null=True, unique=True)
+    # Sets the genre based on a list of options
+    genre = models.CharField(max_length=50, choices=GENRE)
+    # Sets the kid-friendly param to whether it's kid friendly or not
+    kid_friendly = models.CharField(blank=True, max_length=50, choices=FRIENDLY)
+    # Sets the status to a dropdown based on the choices above
+    status = models.IntegerField(choices=STATUS, null=True)
+    # Sets the description to a textfield with max length of 500 words, allows for it to be blank
+    description = models.TextField(max_length=500, blank=True)
+    # Sets the price to 0 by default, and sets the max decimal places to 2
+    price = models.DecimalField(default=0, max_digits=15, decimal_places=2)
