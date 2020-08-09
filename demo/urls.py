2f47ce9d4473486bfc962bcbf347c7ac379ac968
Hung Ly <hunghly@gmail.com>
2020-08-09 02:02:00 -0500

    tested adding a class to django as a view

--- demo/urls.py
+++ demo/urls.py
@@ -1,8 +1,10 @@
 
 from django.urls import path
-from demo import views
+from . import views
+from .views import Another
 
 urlpatterns = [
     path('', views.first),
+    path('another', Another.as_view()),
     path('hello', views.hello)
 ]
--- demo/views.py
+++ demo/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 
-# Create your views here.
+
+
+# Adding a class
+class Another(View):
+
+    def get(self, request):
+        return HttpResponse("This is another function inside class")
+
+    def hello(self, request):
+        return HttpResponse("Hello from another view")
+
+
 def first(request):
     return HttpResponse("First Message from Views")
 
